624821023d7d109f6c746bd959b156069140dba0
Abhishek Kalra <abhishek@Abhisheks-MacBook-Pro.local>
2023-05-25 11:40:11 +0530

    final update

---
+++ blog/hashing.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+pwd_cxt = CryptContext(schemes=["bcrypt"])
+
+class Hash():
+
+    def bcrypt(password : str):
+        
+        return pwd_cxt.hash(password)
+    
+    def verify(hashed_pwd, plain_pwd):
+        return pwd_cxt.verify(plain_pwd, hashed_pwd)
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {'data':{'name': "Abhishek"}}
+@app.get("/blog")
+def index(limit=10,published : bool=True, sort: Optional[str]=None):
+    if published:
+        return {'data':f'{limit}''blog list'}
+    else:
+        return {'data': 'unpublished'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'unpublished'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {'data': f'blog created with title {blog.title}'}
